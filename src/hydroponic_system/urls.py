e7c615aca87a04169232325bc87ea4f8d409764f
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-03-14 15:02:38 +0100

    Add url paths for hydropnic_system app

---
+++ src/hydroponic_system/urls.py
@@ -0,0 +1,14 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+
+from src.hydroponic_system import views as hydroponic_system_views
+
+
+router = DefaultRouter()
+router.register(r"hydroponic_system", hydroponic_system_views.HydroponicSystemViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("measurement/", hydroponic_system_views.PerformMeasurementView.as_view()),
+]
