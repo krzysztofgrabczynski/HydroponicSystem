af7bfa0891d89a8aa69d1156d0fd8d9f1dc8761e
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-03-14 15:01:41 +0100

    Create serializers for measurement perform, create/detail/list hydroponic system

---
+++ src/hydroponic_system/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+
+from src.hydroponic_system.models import HydroponicSystem, Measurement
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ["system", "sensor", "value", "date"]
+        extra_kwargs = {"date": {"read_only": True}}
+
+
+class HydroponicSystemCreationSerializer(serializers.ModelSerializer):
+    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = HydroponicSystem
+        fields = ["owner", "name", "description", "number_of_plants"]
+
+
+class HydroponicSystemDetailSerializer(serializers.ModelSerializer):
+    """
+    Serializer for retrieve user Hydroponic System with information about the measurements taken.
+    """
+
+    measurements = serializers.SerializerMethodField()
+
+    class Meta:
+        model = HydroponicSystem
+        fields = ["owner", "name", "description", "number_of_plants", "measurements"]
+
+    def get_measurements(self, hydroponic_system: HydroponicSystem) -> list:
+        measurements = hydroponic_system.get_measurements()
+        serializer = MeasurementSerializer(measurements, many=True)
+        return serializer.data
+
+
+class HydroponicSystemsListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HydroponicSystem
+        fields = ["name"]
