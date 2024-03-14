4e07cd593e38dc0e4710a9a572d7d942882f9ca8
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-03-14 14:39:14 +0100

    Include new path for hydroponic_system app

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from src.user import urls as user_urls
+from src.hydroponic_system import urls as hydroponic_system_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(user_urls)),
+    path("", include(hydroponic_system_urls)),
 ]
