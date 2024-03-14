417b14bceb15662faf59bc118bfa8dc5936692a6
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-03-14 15:46:32 +0100

    Added flake8 checking in the docker-entrypoint.sh file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,11 @@ echo "Apply database migrations..."
 python manage.py makemigrations
 python manage.py migrate
 
+echo "Checking code with flake8..."
+echo "Start of the flake8 raport..."
+flake8 core
+flake8 src
+echo "End of the flake8 raport..."
+
 echo "Starting server..."
 python manage.py runserver 0.0.0.0:8000 
