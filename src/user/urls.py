2b228d6b3d391c3d9f925c327946468ca0be42e5
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-03-15 15:56:21 +0100

    Added 'name' into urls

--- src/user/urls.py
+++ src/user/urls.py
@@ -4,7 +4,7 @@ from src.user import views as user_views
 
 
 urlpatterns = [
-    path("sign-up/", user_views.UserCreateView.as_view()),
-    path("login/", user_views.LoginView.as_view()),
-    path("logout/", user_views.LogoutView.as_view()),
+    path("sign-up/", user_views.UserCreateView.as_view(), name="sign-up"),
+    path("login/", user_views.LoginView.as_view(), name="login"),
+    path("logout/", user_views.LogoutView.as_view(), name="logout"),
 ]
