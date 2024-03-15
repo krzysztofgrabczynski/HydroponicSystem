c0956aa5f3fe44056d108b9f3b982f7403d1309f
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-03-15 17:49:55 +0100

    New functions for creating hydroponic systems and measurements performance

--- tests/mixins.py
+++ tests/mixins.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+import random
+
+from src.hydroponic_system.models import HydroponicSystem, Measurement, Sensors
 
 
 class CreateUserForTestMixin:
@@ -18,3 +21,25 @@ class LoginUserForTestMixin(CreateUserForTestMixin):
 
         self.token = Token.objects.create(user=self.test_user)
         self.token_header = "Token " + self.token.key
+
+
+def create_hydroponic_system(
+    user: User,
+    name: str = "test_name",
+    description: str = "test_description",
+    number_of_plants: int = 5,
+) -> HydroponicSystem:
+    return HydroponicSystem.objects.create(
+        owner=user,
+        name=name,
+        description=description,
+        number_of_plants=number_of_plants,
+    )
+
+
+def create_measurement(hydroponic_system: HydroponicSystem) -> Measurement:
+    return Measurement.objects.create(
+        system=hydroponic_system,
+        sensor=random.choice(Sensors.values),
+        value=random.randint(0, 100),
+    )
